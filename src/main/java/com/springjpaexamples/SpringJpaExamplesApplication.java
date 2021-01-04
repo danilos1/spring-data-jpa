26917e9614e66921817a0984397a6b425f646f79
Danil <mr.danechek0@gmail.com>
2021-01-04 19:02:07 +0200

    init commit

---
+++ src/main/java/com/springjpaexamples/SpringJpaExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.springjpaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaExamplesApplication.class, args);
+    }
+
+}
